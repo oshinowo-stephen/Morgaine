a77ca3a7cc28ab2fe65b1d0262c80ed52763dbb7
whoastonic <kingstonicthe1@gmail.com>
2021-08-29 03:06:52 -0400

    updated commons tests

--- morg_commons/tests/encrypt.rs
+++ morg_commons/tests/encrypt.rs
@@ -1,22 +1,15 @@
 extern crate morg_commons;
-extern crate async_std;
-
-use std::io;
 
 use morg_commons::encrypt;
 
-#[async_std::test]
-async fn test_hasher() -> io::Result<()> {
+#[test]
+fn test_hasher() {
 	let hash = encrypt::hasher::hash("uwu_kawaii177013"); // A Password I use on a daily basis
 
 	assert!(encrypt::hasher::compare(&hash, "uwu_kawaii177013"));
-
-	Ok(())
 }
 
-#[async_std::test]
-async fn test_random_len() -> io::Result<()> {
-	assert!(encrypt::random::generate_salt().len() == 16);
-
-	Ok(())
+#[test]
+fn test_random_len() {
+	assert!(encrypt::random::generate_salt().len() == 16)
 }
